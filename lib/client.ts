e0f7b696694147d0531deba701d8b70c90e45831
akespresso <94037718+akespresso@users.noreply.github.com>
2023-01-02 13:14:24 -0800

    updates ENV variables

--- lib/client.ts
+++ lib/client.ts
@@ -2,10 +2,12 @@ import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
 import { createClient } from "graphql-ws";
 
-const SUBSCRIPTION_URL = process.env.HASURA_SUBSCRIPTION_URL as string;
-const HASURA_URL = process.env.HASURA_URL as string;
+const SUBSCRIPTION_URL = process.env
+  .NEXT_PUBLIC_HASURA_SUBSCRIPTION_URL as string;
+const HASURA_URL = process.env.NEXT_PUBLIC_HASURA_URL as string;
 
 // WorkAround?
+console.log(process.env.HASURA_SUBSCRIPTION_URL);
 const wsLink = () => new GraphQLWsLink(createClient({ url: SUBSCRIPTION_URL }));
 const httpLink = new HttpLink({ uri: HASURA_URL });
 
